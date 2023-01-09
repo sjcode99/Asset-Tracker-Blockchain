5a38f7542b4984acd46855cd5a37ed31218f814a
suraj lal <surajlal261199@gmail.com>
2023-01-09 12:51:19 +0530

    solidity version and functionality changed

--- contracts/AssetTracker.sol
+++ contracts/AssetTracker.sol
@@ -1,5 +1,5 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.4.21;
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.5.16;
 import "./AssetLibrary.sol";
 
 contract AssetTracker {
@@ -8,13 +8,16 @@ contract AssetTracker {
     // state variable
     uint256 public assetCount= 0;
     
+    // create new asset
     function createAsset(
-        string memory _batchNo,
+        uint _batchNo,
         string memory _name,
         string memory _description,
         string memory _manufacturer,
         string memory _owner,
-        string memory _status
+        string memory _status,
+        string memory _latitude,
+        string memory _longitude
         ) public{
         
         assetCount++;
@@ -27,29 +30,51 @@ contract AssetTracker {
         assSt.statusCount = 0;
         // AssetStore[assetCount]= AssetLibrary.Asset(assetCount, _batchNo, _name, _description, _manufacturer , 0);
         AssetStore[assetCount].statusCount++;
-        AssetStore[assetCount].status[AssetStore[assetCount].statusCount]=AssetLibrary.Status(block.timestamp, _status, _owner);
+        AssetStore[assetCount].articleStatus[AssetStore[assetCount].statusCount]=AssetLibrary.StatusDetail(block.timestamp, _status, _owner, _latitude, _longitude);
         emit AssetCreate(assetCount, _manufacturer, _status);
     }
     
 
-    
-    function getAsset(uint _id) view public returns(string memory, string memory, string memory, string memory, string memory, string memory) {
-        return (AssetStore[_id].batchNo, AssetStore[_id].name, AssetStore[_id].description, AssetStore[_id].manufacturer, AssetStore[_id].status[AssetStore[_id].statusCount].owner, AssetStore[_id].status[AssetStore[_id].statusCount].status);
+    // get info of asset
+    function getAsset(uint _id) view public returns(uint, string memory, string memory, string memory, uint) {       
+        AssetLibrary.Asset storage asst = AssetStore[_id];
+        return (asst.batchNo,
+        asst.name,
+        asst.description, 
+        asst.manufacturer,
+        asst.statusCount);
     }
+
     
-    function transferAsset(uint _id, string memory _newOwner, string memory _status) public{
+    // function getAsset(uint _id) view public returns(uint, string memory, string memory, string memory, string memory, uint,string memory, string memory, string memory) {
+    //     AssetLibrary.Asset storage asst = AssetStore[_id];
+    //     return (asst.batchNo,
+    //     asst.name,
+    //     asst.description, 
+    //     asst.manufacturer, 
+    //     asst.articleStatus[asst.statusCount].owner, 
+    //     asst.articleStatus[asst.statusCount].time,
+    //     asst.articleStatus[asst.statusCount].currentStatus,
+    //     asst.articleStatus[asst.statusCount].latitude,
+    //     asst.articleStatus[asst.statusCount].longitude);
+    // }
+    
+    // transfer asset
+    function transferAsset(uint _id, string memory _newOwner, string memory _status, string memory _latitude, string memory _longitude) public{
         AssetStore[_id].statusCount++;
-        AssetStore[_id].status[AssetStore[_id].statusCount]=AssetLibrary.Status(block.timestamp, _status, _newOwner);
+        AssetStore[_id].articleStatus[AssetStore[_id].statusCount]=AssetLibrary.StatusDetail(block.timestamp, _status, _newOwner,_latitude, _longitude );
         emit AssetTransfer(_id, _newOwner);
     }
     
+    // get total asset count
     function getAssetCount() view public returns(uint) {       
         return assetCount;
     }
     
-    function getStatus(uint _id, uint _statusCount) view public returns(uint, string memory, string memory) {
-        AssetLibrary.Status memory s= AssetStore[_id].status[_statusCount];
-        return (s.time, s.status, s.owner);
+    
+    function getStatus(uint _id, uint _statusCount) view public returns(uint, string memory, string memory, string memory, string memory) {
+        AssetLibrary.StatusDetail memory s= AssetStore[_id].articleStatus[_statusCount];
+        return (s.time, s.currentStatus, s.owner, s.latitude, s.longitude);
     }
     
     event AssetCreate(uint id, string manufacturer, string status);
